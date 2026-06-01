911d0a51ed1e5af8390f36ad6f40552070bb31be
fofackClinton <fofackclinton23@icloud.com>
2026-06-01 09:39:32 +0200

    ajout du preAuthorize pour restrindre le role

--- src/main/java/com/AppJoke/joke/controller/UserConroller.java
+++ src/main/java/com/AppJoke/joke/controller/UserConroller.java
@@ -3,6 +3,7 @@ package com.AppJoke.joke.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,7 @@ public class UserConroller {
     UserService userService;
 
     @GetMapping("all")
+    @PreAuthorize("hasAuthority('ADMIN')")  
     public List<User> getAllUsers() {
         return userService.findAllUsers();
     }
